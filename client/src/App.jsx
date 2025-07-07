3745e1628be5a67aeaaedac0f79224b2b515177b
Rafael Scalet <114953004+rafaScalet@users.noreply.github.com>
2025-07-07 19:07:15 -0300

    add server and client

---
+++ client/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <main className="flex h-screen max-w-screen items-center justify-center bg-ctp-base font-bold font-mono text-ctp-lavender text-xl">
+      <h1>Hello World</h1>
+    </main>
+  );
+}
---
+++ server/src/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 1234;
+
+app.get("/", (_, res) => {
+	res.send({ hello: "world" });
+});
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`);
+});
